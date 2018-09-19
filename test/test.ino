452c0ea9d09802cd1d808ee396404f4e8844d1ca
pi <raspberry>
2018-09-19 12:11:44 +0300

    initial files

---
+++ test/test.ino
@@ -0,0 +1,48 @@
+
+#include <Servo.h>
+
+int servoPin = 9;
+int n;
+
+Servo Servo1;
+
+const int analog = A0;
+const int digital = 3;
+
+void setup() {  
+  Serial.begin(9600);
+  pinMode(digital, OUTPUT);
+  digitalWrite(digital, LOW);
+  Servo1.attach(servoPin);
+  n = 0;
+}
+
+void loop() {
+  if(Serial.available()){
+    n = Serial.read() - '0';
+  }
+  int sensorVal = analogRead(analog);
+
+  float voltage = (sensorVal / 1024.0) * 5.0; 
+
+  // The sensor changes 10 mV per degree (500 mV offset)
+  int temperature = (voltage - .5) * 100;
+ 
+  Serial.println(temperature);
+
+  if (temperature > 25) {
+    digitalWrite(digital, HIGH);
+  }
+
+  else {
+    digitalWrite(digital, LOW);
+  }
+  
+  // Make servo go to n degrees 
+  Servo1.write(n);
+  
+  delay(2000);
+  
+
+
+}
